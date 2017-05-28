bb8deb1487fead15b1bf1123d01680210a2c07d3
livos <lvivancos@gmail.com>
2017-05-28 11:04:56 +0700

    ok

---
+++ app/src/main/java/com/example/bottomsheetdemo/ExampleBottomDialogFragment.java
@@ -0,0 +1,45 @@
+package com.example.bottomsheetdemo;
+
+import android.app.Dialog;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomSheetBehavior;
+import android.support.design.widget.BottomSheetDialogFragment;
+import android.support.design.widget.CoordinatorLayout;
+import android.view.View;
+
+/**
+ * Created by laurent on 5/28/17.
+ */
+
+public class ExampleBottomDialogFragment extends BottomSheetDialogFragment {
+
+    private BottomSheetBehavior.BottomSheetCallback mBottomSheetBehaviorCallback = new BottomSheetBehavior.BottomSheetCallback() {
+
+        @Override
+        public void onStateChanged(@NonNull View bottomSheet, int newState) {
+            if (newState == BottomSheetBehavior.STATE_HIDDEN) {
+                dismiss();
+            }
+
+        }
+
+        @Override
+        public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+        }
+    };
+
+    @Override
+    public void setupDialog(Dialog dialog, int style) {
+       // super.setupDialog(dialog, style);
+        View contentView = View.inflate(getContext(), R.layout.fragment_example_bottom_dialog, null);
+        dialog.setContentView(contentView);
+
+        CoordinatorLayout.LayoutParams params = (CoordinatorLayout.LayoutParams) ((View) contentView.getParent()).getLayoutParams();
+        CoordinatorLayout.Behavior behavior = params.getBehavior();
+
+        if( behavior != null && behavior instanceof BottomSheetBehavior ) {
+            ((BottomSheetBehavior) behavior).setBottomSheetCallback(mBottomSheetBehaviorCallback);
+        }
+    }
+
+}
